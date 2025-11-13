8c1d8ead11edaf64b46eb091ac9b2bb572c54422
RafaorF <101980488+RafaorF@users.noreply.github.com>
2025-11-13 17:54:11 +0000

    Update payments.py

--- cogs/payments.py
+++ cogs/payments.py
@@ -65,7 +65,7 @@ class Payments(commands.Cog):
             
             # Criar sessão de checkout
             checkout_session = stripe.checkout.Session.create(
-                payment_method_types=['card', 'paypal'],
+                payment_method_types=['card'],
                 line_items=[{
                     'price_data': {
                         'currency': moeda.lower(),
@@ -373,3 +373,4 @@ class PaymentView(discord.ui.View):
 async def setup(bot):
 
     await bot.add_cog(Payments(bot))
+
